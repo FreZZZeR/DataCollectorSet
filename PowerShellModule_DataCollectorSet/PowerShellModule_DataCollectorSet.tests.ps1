974aecbf31ddce19dca0073413593ac26f5fd137
ititov02 <ititov02@BIA-025>
2018-02-07 14:32:20 +0300

    Added Pester test but it isn't working

--- PowerShellModule_DataCollectorSet/PowerShellModule_DataCollectorSet.tests.ps1
+++ PowerShellModule_DataCollectorSet/PowerShellModule_DataCollectorSet.tests.ps1
@@ -4,13 +4,20 @@
 # You can download Pester from http://go.microsoft.com/fwlink/?LinkID=534084
 #
 
-Describe "Get-Function" 
+Describe "Test Function Start-DataCollectorSet" 
 {
 	Context "Function Exists" 
 	{
-		It "Should Return" 
+		It "Should Return Status 1" 
 		{
-			
+			Import-Module .\PowerShellModule_DataCollectorSet.psm1
+			$DCSName = "Test"
+			$Computer = "localhost"
+			Start-DCS -CN $Computer -DCSName $DCSName -Force
+			$PerfMonDataCollectorSet = New-Object -ComObject Pla.DataCollectorSet
+			$PerfMonDataCollectorSet.Query($DCSName, $Computer)
+			$Result = $PerfMonDataCollectorSet.Status()
+			$Result | Should Be "1"
 		}
 	}
 }
