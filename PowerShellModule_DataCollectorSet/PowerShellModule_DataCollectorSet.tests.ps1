cdf3b6c90bd3da95a567ccd89bf0c7cfcef87d1f
ititov02 <ititov02@BIA-025>
2018-02-07 14:44:58 +0300

    Info was changed

--- PowerShellModule_DataCollectorSet/PowerShellModule_DataCollectorSet.tests.ps1
+++ PowerShellModule_DataCollectorSet/PowerShellModule_DataCollectorSet.tests.ps1
@@ -1,19 +1,15 @@
 #
 # This is a PowerShell Unit Test file.
-# You need a unit test framework such as Pester to run PowerShell Unit tests. 
-# You can download Pester from http://go.microsoft.com/fwlink/?LinkID=534084
 #
-
 Describe "Test Function Start-DataCollectorSet" 
 {
 	Context "Function Exists" 
 	{
 		It "Should Return Status 1" 
 		{
-			Import-Module .\PowerShellModule_DataCollectorSet.psm1
 			$DCSName = "Test"
 			$Computer = "localhost"
-			Start-DCS -CN $Computer -DCSName $DCSName -Force
+			Start-DataCollectorSet -CN $Computer -DCSName $DCSName -Force
 			$PerfMonDataCollectorSet = New-Object -ComObject Pla.DataCollectorSet
 			$PerfMonDataCollectorSet.Query($DCSName, $Computer)
 			$Result = $PerfMonDataCollectorSet.Status()
